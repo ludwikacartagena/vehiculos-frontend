c594888b9d94e1eb4e19f7039ddaf424fa5284e7
Ludwika <ludwika.brc@gmail.comm>
2025-11-09 05:36:51 -0600

    Update API URL for production

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://vehiculos-backend-production.up.railway.app/api';
 
 export const vehiculosAPI = {
   getAll: () => axios.get(`${API_URL}/vehiculos`),
